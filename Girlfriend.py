da71f206a31b90a878efe52b3eeffe82639293bd
Alexis <alexisbensoussan06@gmail.com>
2022-02-09 00:12:20 +0100

    typo fixes

--- Girlfriend.py
+++ Girlfriend.py
@@ -1,13 +1,15 @@
-'''File that handles all the girlfriend's internal logic, or the lack of per-se.'''
+"""File that handles all the girlfriend's internal logic, or the lack of per-se."""
+
 
 class GirlfriendProperty:
     label: str
-    value: str # Temporary
+    value: str  # Temporary
+
     def __init__(self, label: str, value: str) -> None:
         pass
 
     def apply(girlfriend: 'Girlfriend'):
-      ...
+        ...
 
 
 class HairColor(GirlfriendProperty):
@@ -16,13 +18,13 @@ class HairColor(GirlfriendProperty):
 
 
 class BreastSize(GirlfriendProperty):
-      def __init__(self, value: str) -> None:
+    def __init__(self, value: str) -> None:
         super().__init__('breast_size', value)
+
 
 big_tiddies = BreastSize('91E')
 
+
 class Girlfriend:
-    '''Base Girlfriend class. Used by all your possible girfriends.'''
+    """Base Girlfriend class. Used by all your possible girlfriends."""
     ...
-
-
