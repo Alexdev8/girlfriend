3efcc55af0b83ab36aec0d7ae1439e2a33bdae2f
Vitaly <50383391+Neikow@users.noreply.github.com>
2022-02-09 00:08:02 +0100

    implemented `big_tiddies`

--- Girlfriend.py
+++ Girlfriend.py
@@ -1,5 +1,28 @@
 '''File that handles all the girlfriend's internal logic, or the lack of per-se.'''
 
+class GirlfriendProperty:
+    label: str
+    value: str # Temporary
+    def __init__(self, label: str, value: str) -> None:
+        pass
+
+    def apply(girlfriend: 'Girlfriend'):
+      ...
+
+
+class HairColor(GirlfriendProperty):
+    def __init__(self, value: str) -> None:
+        super().__init__('hair_color', value)
+
+
+class BreastSize(GirlfriendProperty):
+      def __init__(self, value: str) -> None:
+        super().__init__('breast_size', value)
+
+big_tiddies = BreastSize('91E')
+
 class Girlfriend:
-  '''Base Girlfriend class. Used by all your possible girfriends.'''
-  ...
+    '''Base Girlfriend class. Used by all your possible girfriends.'''
+    ...
+
+
